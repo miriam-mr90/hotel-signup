836e3ed73c2c5e23b1b31998cdceeac006aa76ae
Miriam <miriam.mr90@gmail.com>
2019-04-16 14:52:45 +0200

    Refactor: Update Check Animate component to stateless function component

--- hotel-signup/src/components/CheckAnimate/CheckAnimate.js
+++ hotel-signup/src/components/CheckAnimate/CheckAnimate.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './CheckAnimate.scss'
 
-class CheckAnimate extends Component {
-
-  render() {
-    return (
-      <svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 130.2 130.2">
-        <circle
-          className="path circle"
-          fill="none"
-          stroke="#FFF"
-          strokeWidth="6"
-          strokeMiterlimit="10"
-          cx="65.1" cy="65.1"
-          r="62.1" />
-        <polyline
-          className="path check"
-          fill="none"
-          stroke="#FFF"
-          strokeWidth="6"
-          strokeLinecap="round"
-          strokeMiterlimit="10"
-          points="100.2,40.2 51.5,88.8 29.8,67.5 " />
-      </svg>
-    )
-  }
+const CheckAnimate = ({className}) => {
+  return (
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 130.2 130.2"
+      className={className}>
+      <circle
+        className="path circle"
+        fill="none"
+        stroke="#FFF"
+        strokeWidth="6"
+        strokeMiterlimit="10"
+        cx="65.1" cy="65.1"
+        r="62.1" />
+      <polyline
+        className="path check"
+        fill="none"
+        stroke="#FFF"
+        strokeWidth="6"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+        points="100.2,40.2 51.5,88.8 29.8,67.5 " />
+    </svg>
+  )
 }
 
 export default CheckAnimate;
